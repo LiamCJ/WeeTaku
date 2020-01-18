60eacad8629de562e4464f48ae655d172c44dcc9
LiamCJ <liamcjones2000@gmail.com>
2020-01-18 10:41:37 +0200

    Functionality Given To Edit And Delete Methods

--- src/WeeTaku.java
+++ src/WeeTaku.java
@@ -49,32 +49,40 @@ public class WeeTaku {
                     mySmt.executeUpdate(command);
                     
                     System.out.println("Added!!");
+                    break;
                     
                 case 2:
                     mySmt.executeUpdate(command);
                     
                     System.out.println("Changes Made");
+                    break;
                     
                 case 3:
                     ResultSet mySearch = mySmt.executeQuery(command);
-                
-                    System.out.println(mySearch.getString("Title") + " | " + mySearch.getString("Last_Episode_Watched") + " | " +  mySearch.getString("Last_Season_Watched") + " | " +  mySearch.getString("Number_of_Seasons"));
+
+                    while (mySearch.next()){
+                     System.out.println(mySearch.getString("Title") + " | " + mySearch.getString("Last_Episode_Watched") + " | " +  mySearch.getString("Last_Season_Watched") + " | " +  mySearch.getString("No_of_Seasons"));
+                    }
+                    break;
+                    
                 case 4:
                     ResultSet myRes = mySmt.executeQuery(command);
 
                     while (myRes.next()){
-                     System.out.println(myRes.getString("Title") + " | " + myRes.getString("Last_Episode_Watched") + " | " +  myRes.getString("Last_Season_Watched") + " | " +  myRes.getString("Number_of_Seasons"));
+                     System.out.println(myRes.getString("Title") + " | " + myRes.getString("Last_Episode_Watched") + " | " +  myRes.getString("Last_Season_Watched") + " | " +  myRes.getString("No_of_Seasons"));
                     }
+                    break;
                     
                 case 5:
-                    mySmt.executeQuery(command);
+                    mySmt.executeUpdate(command);
                     
                     System.out.println("Deleted Series");
+                    break;
                 
-//                default:
-//                    System.out.println("Enter Numbers From 1-5");
+                default:
 //                    Menu();
-                
+                    System.out.println("Enter Numbers From 1-5");
+                   
            }
            
            
@@ -91,8 +99,7 @@ public class WeeTaku {
         System.out.println("1. Add Series To Libary \n2. Edit Series \n3. Find Series \n4. View All \n5. Delete Series\n6. Exit");
         userIn = input.nextInt();
         
-        menuCommands.get(userIn).run();
-               
+        menuCommands.get(userIn).run();  
     }
     
     //Method To Add A New Series
@@ -122,28 +129,39 @@ public class WeeTaku {
         System.out.println("_____________Weetaku: Edit Series_____________\n");
         title = input.nextLine();
         System.out.print("Enter Series Title You Want To Change: ") ;
-        System.out.println("What Is The Details Of The Series You Would Like To Edit?(PLEASE ENTER A NUMBER FROM 1-5)");
-        System.out.println("1. Title\n2. Last Episode Watched\n3. Last Season Watched\n4. Number of Seasons\n5. Back To Menu");
+        title = input.nextLine();
+        System.out.println("What Is The Details Of The Series You Would Like To Edit?(PLEASE ENTER A NUMBER FROM 1-4)");
+        System.out.println("1. Title\n2. Last Episode Watched\n3. Last Season Watched\n4. Number of Seasons");
         userIn = input.nextInt();
         
         switch(userIn){
             case 1:
                 detail = "Title";
-                System.out.println("What Would You Like To Change It To?: ");
+                changedDetail = input.nextLine();
+                System.out.println("What Would You Like To Change The Title To?: ");
+                changedDetail = input.nextLine();
+                break;
             case 2:
                 detail = "Last_Episode_Watched";
-                System.out.println("What Would You Like To Change It To?: ");
+                changedDetail = input.nextLine();
+                System.out.println("What Would You Like To Change The Last Episode Watched To?: ");
+                changedDetail = input.nextLine();
+                break;
             case 3:
                 detail = "Last_Season_Watched";
-                System.out.println("What Would You Like To Change It To?: ");
+                changedDetail = input.nextLine();
+                System.out.println("What Would You Like To Change The Last Season Watched To?: ");
+                changedDetail = input.nextLine();
+                break;
             case 4:
                 detail = "No_of_Seasons";
-                System.out.println("What Would You Like To Change It To?: ");
-            case 5:
-                Menu();
+                changedDetail = input.nextLine();
+                System.out.println("What Would You Like To Change The Number of Seasons To?: ");
+                changedDetail = input.nextLine();
+                break;
             default:
                 editSeries();
-                System.out.println("Please Enter Number From 1-5!!");
+                System.out.println("PLEASE ENTER A NUMBER FROM 1-4");
                 
         }
         
@@ -170,10 +188,11 @@ public class WeeTaku {
     //Method That Allows User To Delete Series
     public static void deleteSeries(){
         clearTerminal();
-        System.out.println("_____________Weetaku: Put Series In Order_____________\n");
-        System.out.println("What Order Would You Like Your Series In(PLEASE ENTER A NUMBER FROM 1-5)");
-        System.out.println("1. Aplhabetacal\n2. Last Watched\n3. No_of_Seasons\n4. ID\n5. Back To Menu");
-        userIn = input.nextInt();
+        userStr = input.nextLine();
+        System.out.print("Please Enter The Title of The Series You Wish To Delete: ");
+        userStr = input.nextLine();
+        String db_command  = String.format("delete from Seriesous where Title = '%s'", userStr); 
+        sqlCommands(db_command, 5);
     }
      
     public static void main(String[] args){
